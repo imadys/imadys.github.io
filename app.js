df3b53c5aded0aa2dbd8bb65ad1326d174a25c1a
imadys <47067573+imadys@users.noreply.github.com>
2019-08-22 17:52:18 +0300

    jokes

--- app.js
+++ app.js
@@ -1,113 +1,33 @@
-// Book Constructor
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-}
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
 
+  const xhr = new XMLHttpRequest();
 
-// UI Constructor
-function UI() { }
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-// add book to list
-UI.prototype.addBookToList = function (book) {
-  const list = document.getElementById('book-list');
-  // Create tr element
-  const row = document.createElement('tr');
-  // insert cols
-  row.innerHTML = `
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.isbn}</td>
-  <td><a href="#" class="delete">X</a></td>
-  `;
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
 
-  list.appendChild(row);
-}
+      let output = '';
 
-// show alert
-UI.prototype.showAlert = function (message, className) {
-  // create div
-  const div = document.createElement('div');
-  // add classes
-  div.className = `alert ${className}`;
-  // add text
-  div.appendChild(document.createTextNode(message));
-  //get parent
-  const container = document.querySelector('.container');
-  // get form
-  const form = document.querySelector('#book-form');
-  // insert alert
-  container.insertBefore(div, form);
-  // time out after 3 sec
-  setTimeout(function () {
-    document.querySelector('.alert').remove();
-  }, 3000);
+      if (response.type === 'success') {
+        response.value.forEach(function (joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
 
-}
-// delete book
-UI.prototype.deleteBook = function (target) {
-  if (target.className === 'delete') {
-    target.parentElement.parentElement.remove();
-  }
-}
-// clear fields
-UI.prototype.clearFields = function () {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
-
-}
-
-
-
-// Event Listeners add book
-document.getElementById('book-form').addEventListener('submit',
-  function (e) {
-    // get form values
-    const title = document.getElementById('title').value,
-      author = document.getElementById('author').value,
-      isbn = document.getElementById('isbn').value
-
-    // Instantiate book
-    const book = new Book(title, author, isbn);
-
-    // Instantiate UI
-    const ui = new UI();
-
-    // vaildate
-    if (title === '' || author === '' || isbn === '') {
-      // row.innerHTML = '';
-      // error alert
-      ui.showAlert('Please fill in all fields', 'error');
-    } else {
-
-
-      // add book to list
-      ui.addBookToList(book);
-
-      // show success
-      ui.showAlert('Book Added!', 'success');
-
-      // Clear fields
-      ui.clearFields();
+      document.querySelector('.jokes').innerHTML = output;
 
     }
 
+  }
 
-    e.preventDefault();
-  });
-
-// event listner for delete
-
-document.getElementById('book-list').addEventListener('click', function (e) {
-
-  const ui = new UI();
-  ui.deleteBook(e.target);
-
-  // show message
-  ui.showAlert('Book removed', 'success');
+  xhr.send();
 
   e.preventDefault();
-});
+}
