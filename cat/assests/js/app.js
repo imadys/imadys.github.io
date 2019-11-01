8762f22317097656646e3998bd4db4143086bdb2
imadys <47067573+imadys@users.noreply.github.com>
2019-11-02 01:28:56 +0300

    deleted stuff

--- cat/assests/js/app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navBar = document.querySelector('.navbar');
-const links = document.querySelector('.navbar li');
-
-hamburger.addEventListener('click', () => {
-  navBar.classList.toggle('open');
-});
